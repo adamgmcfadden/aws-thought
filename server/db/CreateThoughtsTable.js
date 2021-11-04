44aa0ad25bf511aa84801e1eb8bd900cd38119a7
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-11-04 15:09:45 -0400

    database integrated into the front-end

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -4,7 +4,6 @@ const AWS = require("aws-sdk");
 // modify the AWS config object that DynamoDB will use to connect to the local instance
 AWS.config.update({
   region: "us-east-2",
-  endpoint: "http://localhost:8000",
 });
 
 // create the DynamoDb service object
